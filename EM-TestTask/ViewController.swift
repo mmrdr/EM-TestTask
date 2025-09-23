f5cc37c0715c5a55141452d2c4e3c035cce9ea14
mmrdr <isaev2k18@gmail.com>
2025-09-23 13:43:53 +0300

    Init commit

---
+++ EM-TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EM-TestTask
+//
+//  Created by Кирилл Исаев on 23.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
